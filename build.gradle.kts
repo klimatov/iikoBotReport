0844085b1ab73de9df1e63f4c00ea11e5f4da524
klimatov <klimatov@mail.ru>
2022-06-17 22:42:49 +0700

    v 0.1 - Коннект к серверу и получение кукис - Получение отчета по ID и датам начала и конца - Замена "." на "-" в тегах ответа для корректной обработки JSOUP-ом - Парсинг таблицы с данными отчета

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.20"
+    application
+}
+
+group = "com.github.klimatov"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("dev.inmo:tgbotapi:1.1.0")
+    implementation("org.jsoup:jsoup:1.14.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
