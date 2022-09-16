56159c9bab248ee772ec68e770b26205e9335968
klimatov <klimatov@mail.ru>
2022-09-16 19:39:09 +0700

    v 0.16 + текстовое поле с кнопкой СОХРАНИТЬ на странице списка отчетов (заметки)

---
+++ src/main/kotlin/data/fileProcessing/NotesRepository.kt
@@ -0,0 +1,25 @@
+package data.fileProcessing
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import fileProcessing.FileOperations
+
+class NotesRepository {
+    private val tag = this::class.java.simpleName
+    fun get(): String {
+        val serializedData = FileOperations().read("notes.txt")
+        val type = object : TypeToken<String>() {}.type
+        var notes =
+            Gson().fromJson<String>(serializedData, type)
+        if (notes == null) notes = ""
+        return notes
+    }
+
+    fun set(notes: String?) {
+        if (notes != null) {
+            val serializedData = Gson().toJson(notes)
+            FileOperations().write("notes.txt", serializedData)
+        }
+    }
+
+}
--- src/main/kotlin/webServer/Routing.kt
+++ src/main/kotlin/webServer/Routing.kt
@@ -1,12 +1,17 @@
 package webServer.plugins
 
+import data.fileProcessing.NotesRepository
 import data.fileProcessing.WorkersRepository
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.html.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.util.*
 import kotlinx.html.*
+import utils.Logging
 
 fun Application.configureRouting() {
     val tag = "configureRouting"
@@ -21,6 +26,7 @@ fun Application.configureRouting() {
                     }
                     body {
                         val workerList = WorkersRepository().get()
+                        val notes = NotesRepository().get()
 
                         +"Список активных отчетов:"
                         br()
@@ -35,8 +41,39 @@ fun Application.configureRouting() {
                         a(href = "edit-worker") {
                             +"Создать новый отчет"
                         }
+
+                        repeat(3) { br() }
+
+                        postForm {
+                            + "Заметки:"
+                            br()
+                            textArea{
+                                name = "notes"
+                                rows = "20"
+                                cols = "80"
+                                +notes
+                            }
+
+                            br()
+                            button(type = ButtonType.submit) {
+                                name = "saveButton"
+                                +"СОХРАНИТЬ"
+                            }
+                        }
                     }
                 }
+            }
+
+            post("/") {
+                val receiveParam: Map<String, List<String>> = call.receiveParameters().toMap()
+                Logging.d(tag,receiveParam.toString())
+                val notes = receiveParam["notes"]?.joinToString()
+
+                if (receiveParam.containsKey("saveButton")) {                                   // - SAVE !!!
+                    Logging.i(tag,"Нажата кнопка SAVE")
+                    NotesRepository().set(notes)
+                }
+                call.respondRedirect("/")
             }
         }
     }
